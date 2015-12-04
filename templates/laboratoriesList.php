620fbd5038f63fe51b99485299138d8589a7b5f9
Jaime Francisco Duarte Pinzón <jaimeduarte2411@hotmail.com>
2015-12-03 23:09:41 -0700

    'Second'

---
+++ templates/laboratoriesList.php
@@ -0,0 +1,47 @@
+<?php
+?>
+
+<head>
+	<title> forMaestro</title>
+	<meta charset="UTF-8">
+
+</head>
+
+<body>
+
+  <div id="contenedor">
+
+		<center>
+			<h1  id="tituloFB"> Laboratorios Rejistrados </h1>
+		</center>
+	<!--<style type="text/css">
+				.tftable {font-size:20px;color:#333333;width:150%;border-width: 2px;border-color: #729ea5;border-collapse: collapse;}
+				.tftable th {font-size:20px;background-color:#acc8cc;border-width: 2px;padding: 10px;border-style: solid;border-color: #729ea5;text-align:left;}
+				.tftable tr {background-color:#d4e3e5;}
+				.tftable td {font-size:20px;border-width: 2px;padding: 10px;border-style: solid;border-color: #729ea5;}
+				.tftable tr:hover {background-color: #729CA9;}
+
+	</style>!-->
+							<link rel="stylesheet" type="text/css" href="css/Style.css">
+
+
+		
+
+			<table class="Tabla" border="6">
+			<tr><th>Laboratorio</th><th>Capacidad De Alumnos</th><th>Salon</th><th>Capacidad De Alumnos</th></tr>
+			<tr><td>Lab A </td><td>30</td><td>SC1</td><td>40</td>
+			<tr><td>Lab B </td><td>35</td><td>SC2</td><td>45</td>
+			<tr><td>Lab C </td><td>40</td><td>SC3</td><td>35</td>
+			<tr><td>Lab D </td><td>45</td><td>SC4</td><td>40</td>
+			<tr><td>Lab E </td><td>50</td><td>SC5</td><td>40</td>
+			<tr><td> </td><td></td><td></td><td></td>
+
+			</table>
+		
+	  
+		      <input id='boton' type='submit' value='Back '/> 
+	    </center>     
+  </div>
+
+</body>
+
---
+++ templates/teachersList.php
@@ -0,0 +1,47 @@
+<?php
+?>
+
+<head>
+	<title> forMaestro</title>
+	<meta charset="UTF-8">
+
+</head>
+
+<body>
+
+  <div id="contenedor">
+
+		<center>
+			<h1  id="tituloFB"> Informacion De Maestros </h1>
+		</center>
+	<!--<style type="text/css">
+				.tftable {font-size:20px;color:#333333;width:150%;border-width: 2px;border-color: #729ea5;border-collapse: collapse;}
+				.tftable th {font-size:20px;background-color:#acc8cc;border-width: 2px;padding: 10px;border-style: solid;border-color: #729ea5;text-align:left;}
+				.tftable tr {background-color:#d4e3e5;}
+				.tftable td {font-size:20px;border-width: 2px;padding: 10px;border-style: solid;border-color: #729ea5;}
+				.tftable tr:hover {background-color: #729CA9;}
+
+	</style>!-->
+							<link rel="stylesheet" type="text/css" href="css/Style.css">
+
+
+		
+
+			<table class="Tabla" border="5">
+			<tr><th>Nombre</th><th>Apellido Paterno</th><th>Apellido Materno</th><th>Matricula</th></tr>
+			<tr><td>Jesus </td><td>Hernandez</td><td>Cosio</td><td>374892</td>
+			<tr><td>Alejandro</td><td>Leyva</td><td>Carrillo</td><td>364791</td>
+			<tr><td>Jesus Antonio</td><td>Rodriguez</td><td>Meza</td><td>354690</td>
+			<tr><td>Victor Manuel</td><td>Dominguez</td><td>Cota</td><td>344589</td>
+			<tr><td>Guadalupe</td><td>Ordaz</td><td>Picos</td><td>334488</td>
+			<tr><td></td><td></td><td></td><td></td>
+
+			</table>
+		
+	  
+		      <input id='boton' type='submit' value='Back '/> 
+	    </center>     
+  </div>
+
+</body>
+
